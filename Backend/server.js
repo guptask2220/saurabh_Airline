791a4c12862305d980829a63786eb1b8a5a81fef
Saurabh Gupta <166496591+guptask2220@users.noreply.github.com>
2025-04-27 18:21:20 +0530

    Update serverfinal.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://saurabh-airline.vercel.app',
+    origin: 'https://saurabh-airline-3.onrender.com',
     credentials: true,
   }));
   
